c5248261de681fb4bf4b9c9bee8992886bdd1050
costagilberto <costagilberto@users.noreply.github.com>
2022-09-12 09:02:48 -0300

    Add files via upload

---
+++ cassava_ci.py
@@ -0,0 +1,100 @@
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+
+import albumentations as A
+import cv2
+import numpy as np
+from tqdm import tqdm
+
+from utils import check_bbox, get_slice_bboxes, img2label_paths
+
+DATASET_DIR = Path(".").parent
+DATASET_DIRS = [
+    *map(DATASET_DIR.joinpath, ["low_abundance", "moderate_abundance", "super_abundance"])
+]
+
+OUT_DIR = DATASET_DIR.joinpath("CI")
+
+IMG_DIRS = [set / "images" for set in DATASET_DIRS]
+TXT_ANNOTATION_DIR = [set / "annotation_txt" for set in DATASET_DIRS]
+XML_ANNOTATION_DIR = [set / "annotation" for set in DATASET_DIRS]
+
+SLICE_HEIGHT = 128
+SLICE_WIDTH = 128
+SLICE_OV_HEIGHT = 0.0
+SLICE_OV_WIDTH = 0.0
+
+
+def process_single_img(file, label):
+    # Load image
+    img = cv2.imread(str(file))
+    (h, w) = img.shape[:2]
+
+    # Load Labels
+    with open(label, "r") as f:
+        l = [x.split() for x in f.read().strip().splitlines()]
+        l = np.array(l, dtype=np.float32)
+
+    # Drop Labels with no Width or Height
+    drop_l = []
+    for n, obj in enumerate(l):
+        if not np.all(obj[2:]):
+            print(f"{label.name} -- Dropped Label {n}/{len(l)}: {obj}")
+            drop_l.append(n)
+        else:
+            check_bbox(obj[1:], xywh=True)
+    l = np.delete(l, drop_l, axis=0)
+
+    # Compute Slices
+    slices = get_slice_bboxes(h, w, SLICE_HEIGHT, SLICE_WIDTH, SLICE_OV_HEIGHT, SLICE_OV_WIDTH)
+
+    bbox_params = A.BboxParams(format="yolo", label_fields=["labels"], min_visibility=0.5)
+    composed_slices = [A.Compose([A.Crop(*slice)], bbox_params) for slice in slices]  # type: ignore
+    tf_slices = map(lambda x: x(image=img, bboxes=l[:, 1:], labels=l[:, 0]), composed_slices)
+
+    # Save to Output
+    out_p = OUT_DIR / file.parent.relative_to(DATASET_DIR)
+    for n, tf in enumerate(tf_slices):
+        # Save image
+        out_file = out_p / f"{file.stem}_{n}.jpg"
+        cv2.imwrite(str(out_file), tf["image"])
+
+        # Save transformed labels
+        with open(img2label_paths(out_file), "w+") as lb:
+            if tf["bboxes"]:
+                for obj in tf["bboxes"]:
+                    check_bbox(obj, xywh=True)
+                    lb.write(f"0 {obj[0]:.6f} {obj[1]:.6f} {obj[2]:.6f} {obj[3]:.6f}\n")
+
+
+if __name__ == "__main__":
+    # mkdir
+    CI_FOLDERS = IMG_DIRS + TXT_ANNOTATION_DIR
+    for fd in CI_FOLDERS:
+        OUT_DIR.joinpath(fd).mkdir(parents=True, exist_ok=True)
+
+    # image files & labels
+    img_files = []
+    for img_p in IMG_DIRS:
+        img_files += [*img_p.glob("*.jpg")]
+    files = [f.stem for f in img_files]
+    labels = [img2label_paths(img_p, check=True) for img_p in img_files]
+
+    # Run Processing Jobs
+    jobs = []
+    pbar = tqdm(total=len(files), desc="Images")
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        # Submit Jobs
+        for (file, label) in zip(img_files, labels):
+            future = executor.submit(process_single_img, file, label)
+            future.add_done_callback(lambda _: pbar.update(1))
+            jobs.append(future)
+
+        # Wait....
+        try:
+            executor.shutdown(wait=True)
+        except KeyboardInterrupt:
+            executor.shutdown(wait=False, cancel_futures=True)
+        finally:
+            pbar.close()
+    results = [future.result() for future in jobs]
---
+++ utils.py
@@ -0,0 +1,100 @@
+from pathlib import Path
+import numpy as np
+
+from typing import List, Sequence
+
+# modified from https://github.com/obss/sahi/blob/main/sahi/slicing.py
+def get_slice_bboxes(
+    image_height: int,
+    image_width: int,
+    slice_height: int = 128,
+    slice_width: int = 128,
+    overlap_height_ratio: float = 0.0,
+    overlap_width_ratio: float = 0.0,
+) -> List[List[int]]:
+    """Slices `image_pil` in crops.
+    Corner values of each slice will be generated using the `slice_height`,
+    `slice_width`, `overlap_height_ratio` and `overlap_width_ratio` arguments.
+    Args:
+        image_height (int): Height of the original image.
+        image_width (int): Width of the original image.
+        slice_height (int): Height of each slice. Default 512.
+        slice_width (int): Width of each slice. Default 512.
+        overlap_height_ratio(float): Fractional overlap in height of each
+            slice (e.g. an overlap of 0.2 for a slice of size 100 yields an
+            overlap of 20 pixels). Default 0.2.
+        overlap_width_ratio(float): Fractional overlap in width of each
+            slice (e.g. an overlap of 0.2 for a slice of size 100 yields an
+            overlap of 20 pixels). Default 0.2.
+        auto_slice_resolution (bool): if not set slice parameters such as slice_height and slice_width,
+            it enables automatically calculate these params from image resolution and orientation.
+    Returns:
+        List[List[int]]: List of 4 corner coordinates for each N slices.
+            [
+                [slice_0_left, slice_0_top, slice_0_right, slice_0_bottom],
+                ...
+                [slice_N_left, slice_N_top, slice_N_right, slice_N_bottom]
+            ]
+    """
+    slice_bboxes = []
+    y_max = y_min = 0
+
+    if slice_height and slice_width:
+        y_overlap = int(overlap_height_ratio * slice_height)
+        x_overlap = int(overlap_width_ratio * slice_width)
+    else:
+        raise ValueError("Compute type is not auto and slice width and height are not provided.")
+
+    while y_max < image_height:
+        x_min = x_max = 0
+        y_max = y_min + slice_height
+        while x_max < image_width:
+            x_max = x_min + slice_width
+            if y_max > image_height or x_max > image_width:
+                xmax = min(image_width, x_max)
+                ymax = min(image_height, y_max)
+                xmin = max(0, xmax - slice_width)
+                ymin = max(0, ymax - slice_height)
+                slice_bboxes.append([xmin, ymin, xmax, ymax])
+            else:
+                slice_bboxes.append([x_min, y_min, x_max, y_max])
+            x_min = x_max - x_overlap
+        y_min = y_max - y_overlap
+    return slice_bboxes
+
+
+def img2label_paths(img_path, check=False):
+    parts = list(img_path.parts)
+    for n, part in enumerate(reversed(parts)):
+        if part == "images":
+            parts[-(n + 1)] = "labels"
+            break
+    parts[-1] = f"{img_path.stem}.txt"
+
+    label_path = Path(*parts)
+    if check:
+        assert label_path.is_file(), f"Missing label for {img_path}"
+    return label_path
+
+
+def check_bbox(bbox: Sequence, xywh=False) -> None:
+    if xywh:
+        x, y, w, h = bbox
+        w_half, h_half = w / 2, h / 2
+        x_min = x - w_half
+        y_min = y - h_half
+        x_max = x_min + w
+        y_max = y_min + h
+        bbox = [x_min, y_min, x_max, y_max]
+
+    """Check if bbox boundaries are in range 0, 1 and minimums are lesser then maximums"""
+    for name, value in zip(["x_min", "y_min", "x_max", "y_max"], bbox[:4]):
+        if not 0 <= value <= 1 and not np.isclose(value, 0) and not np.isclose(value, 1):
+            raise ValueError(
+                f"Expected {name} for bbox {bbox} " f"to be in the range [0.0, 1.0], got {value}."
+            )
+    x_min, y_min, x_max, y_max = bbox[:4]
+    if x_max <= x_min:
+        raise ValueError(f"x_max is less than or equal to x_min for bbox {bbox}.")
+    if y_max <= y_min:
+        raise ValueError(f"y_max is less than or equal to y_min for bbox {bbox}.")
